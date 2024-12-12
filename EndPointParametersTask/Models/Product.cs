434d0f6aa75fe14d3f1be93293ca10e997988041
Azzak <azzaalmahrooqi79@gmail.com>
2024-12-12 12:50:25 +0400

    add defualt value

--- EndPointParametersTask/Models/Product.cs
+++ EndPointParametersTask/Models/Product.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public string Category { get; set; }
-        public DateTime DateAdded { get; set; }
+        public string Category { get; set; } = "General";
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
     }
 }
