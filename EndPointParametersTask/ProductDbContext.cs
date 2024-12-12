45c34604360728ca13072657b5e8af424aadb90a
Azzak <azzaalmahrooqi79@gmail.com>
2024-12-12 12:57:12 +0400

    create ProductTable

---
+++ EndPointParametersTask/ProductDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EndPointParametersTask.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndPointParametersTask
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
