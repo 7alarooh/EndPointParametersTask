83866e2fc1a652c5ee74e76f89937ab1ea7ed1d5
Azzak <azzaalmahrooqi79@gmail.com>
2024-12-12 12:44:38 +0400

    Create the Product Model

---
+++ EndPointParametersTask/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace EndPointParametersTask.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
