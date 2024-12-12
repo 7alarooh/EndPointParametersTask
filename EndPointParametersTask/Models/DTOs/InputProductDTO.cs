1a3f670df4352c2d5f81d2b666321d62f426f213
Azzak <azzaalmahrooqi79@gmail.com>
2024-12-12 13:00:00 +0400

    Implementation

---
+++ EndPointParametersTask/Models/DTOs/InputProductDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EndPointParametersTask.Models.DTOs
+{
+    public class InputProductDTO
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Category { get; set; } = "General";
+    }
+}
---
+++ EndPointParametersTask/Models/DTOs/OutputProductDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace EndPointParametersTask.Models.DTOs
+{
+    public class OutputProductDTO
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
