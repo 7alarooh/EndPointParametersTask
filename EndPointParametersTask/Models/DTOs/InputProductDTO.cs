5b6ec50836dda4345b2e9e1286e5915879b3c79a
Azzak <azzaalmahrooqi79@gmail.com>
2024-12-12 13:06:27 +0400

    Added InputProductDTO with validation for Name and Price fields

--- EndPointParametersTask/Models/DTOs/InputProductDTO.cs
+++ EndPointParametersTask/Models/DTOs/InputProductDTO.cs
@@ -1,9 +1,16 @@
-﻿namespace EndPointParametersTask.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EndPointParametersTask.Models.DTOs
 {
     public class InputProductDTO
     {
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
         public decimal Price { get; set; }
+
         public string? Category { get; set; } = "General";
     }
 }
