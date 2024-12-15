82c04fd2f2d311046373d67e757c92aab858e674
Azzak <azzaalmahrooqi79@gmail.com>
2024-12-15 15:03:59 +0400

    Create User Table and add data type validation

---
+++ EndPointParametersTask/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace EndPointParametersTask.Models
+{
+    public class User
+    {
+        [Key]
+        [JsonIgnore]
+        public int UId { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Password must be between 4 and 20 characters")]
+        public string Password { get; set; }
+    }
+}
